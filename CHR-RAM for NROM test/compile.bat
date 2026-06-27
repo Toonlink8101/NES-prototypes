8e5bdb6ee1e16cec4ffbb5acc984a2d110a30cc9
Daniel Wichowski <toonlink8101@gmail.com>
2026-06-27 14:58:41 -0500

    CHR-RAM for NROM test

---
+++ CHR-RAM for NROM test/compile.bat
@@ -0,0 +1,17 @@
+@echo.
+@echo Compiling...
+C:\cc65\bin\ca65 cart.s -g -o example.o --cpu 6502X
+@IF ERRORLEVEL 1 GOTO failure
+@echo.
+@echo Linking...
+C:\cc65\bin\ld65 -o NROM-CHR-RAM.nes -C example.cfg example.o
+@IF ERRORLEVEL 1 GOTO failure
+@echo.
+@echo Success!
+@pause
+@GOTO endbuild
+:failure
+@echo.
+@echo Build error!
+@pause
+:endbuild
