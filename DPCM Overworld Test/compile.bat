05ea494f9753b45b6944f43564113bb1473d0c84
Daniel Wichowski <toonlink8101@gmail.com>
2026-07-14 13:56:03 -0500

    Renamed output file

--- DPCM Overworld Test/compile.bat
+++ DPCM Overworld Test/compile.bat
@@ -4,7 +4,7 @@ C:\cc65\bin\ca65 cart.asm -g -o example.o --cpu 6502X
 @IF ERRORLEVEL 1 GOTO failure
 @echo.
 @echo Linking...
-C:\cc65\bin\ld65 -o DMC4AV.nes -C example.cfg example.o
+C:\cc65\bin\ld65 -o "Overworld Test.nes" -C example.cfg example.o
 @IF ERRORLEVEL 1 GOTO failure
 @echo.
 @echo Success!
